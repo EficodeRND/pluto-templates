640a862d1096929287873f2c89f633d7e6d18feb
Mikki Levón <mikki.levon@eficode.com>
2021-11-24 14:51:03 +0200

    response.data instead of response

--- applications/web_app/frontend/react_with_rest/template/src/App.js
+++ applications/web_app/frontend/react_with_rest/template/src/App.js
@@ -9,7 +9,7 @@ function App() {
   useEffect(() => {
     Axios.get(API_URL)
       .then((response) => {
-        setBackendResponse(JSON.stringify(response));
+        setBackendResponse(JSON.stringify(response.data));
       })
       .catch((response) => {
         setBackendResponse(response.status)
