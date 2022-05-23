bd1b0ebd87541949399b5e44c77bd5457b409681
aromu <aapo.romu@eficode.com>
2022-05-23 15:39:14 +0300

    Add example of including external modules to executor scripts

---
+++ executor_libraries/some_utility/tech_util.py
@@ -0,0 +1,5 @@
+import common_util
+
+
+def sum_n_multiply(a: int) -> int:
+    return common_util.add(a, a) * a
--- projects/atlassian/jira/template/create_jira_project.py
+++ projects/atlassian/jira/template/create_jira_project.py
@@ -4,6 +4,7 @@ import os
 import json
 import logging as log
 from utils.common import LogManager
+from some_utility import tech_util
 
 if __name__ == '__main__':
     log_manager = LogManager()
@@ -16,7 +17,8 @@ if __name__ == '__main__':
         'projectKey': 'TSTPRJ',
         'projectName': os.environ['PROJECT_NAME'],
         'boardType': os.environ['BOARD_TYPE'],
-        'url': 'http://jira.yourcompany.example.com'
+        'url': 'http://jira.yourcompany.example.com',
+        'someValue': tech_util.sum_n_multiply(2)
     }}
 
     with open(os.environ['RESULT_FILE'], 'w', encoding="utf8") as target_file:
