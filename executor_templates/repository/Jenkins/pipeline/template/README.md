8d0cab9e5a7533bc6871b6476c9a11ae4ec4a7c9
Emil <emil.heliste@helsinki.fi>
2022-06-13 11:33:13 +0300

    make script talk to jenkins (#2)
    Co-authored-by: Emil Heliste <emil.heliste@eficode.com>

---
+++ executor_templates/repository/Jenkins/pipeline/template/README.md
@@ -0,0 +1,2 @@
+# Setting up a Jenkins CI & CD Pipeline automatically for a GitHub repository
+Make sure you have authorized https://github.com/apps/sprout-jenkins-github-app to setup webhooks on your behalf to your GitHub account & scan your repos.
--- executor_templates/repository/Jenkins/pipeline/template/jenkins.py
+++ executor_templates/repository/Jenkins/pipeline/template/jenkins.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 
+import sys
 import os
 import json
 import logging as log
+import requests
+import base64
 # from utils.common import LogManager
+
+if 'services' in locals():
+    from services.templating.renderer import TemplateRenderer, SimpleTemplateContext
+else:
+    print("no sprout services found")
+    args=sys.argv
+    if args[1]=="debug":
+        PROJECT_NAME, GITHUB_REPO_URL, JENKINS_SERVER_URL, JENKINS_FOLDER_NAME, JENKINS_USERNAME, JENKINS_API_KEY = args[2], args[3], args[4], args[5], args[6], args[7]
 
 if __name__ == '__main__':
   print("RUNNING....")
   # log.info("Environment: %s", os.environ)
 
   dirname = os.path.dirname(__file__)
-  filename = os.path.join(dirname, "jenkins-multibranch-pipeline.xml")
+  filename = os.path.join(dirname, "jenkins-multibranch-pipeline.xml.j2")
 
   if not os.path.exists(filename):
     print("File does not exist.")
@@ -21,3 +32,41 @@ if __name__ == '__main__':
 
     for line in content:
       print(line)
+
+headers = requests.structures.CaseInsensitiveDict()
+headers["Content-Type"] = "application/x-www-form-urlencoded"
+login_string=JENKINS_USERNAME+":"+JENKINS_API_KEY
+ut64 = base64.b64encode(bytes(login_string, encoding='utf8')).decode()
+print("using auth header Basic "+ut64)
+
+headers["Authorization"] = "Basic "+str(ut64)
+
+#get jenkins crumb
+crumb_request_url=JENKINS_SERVER_URL + "crumbIssuer/api/json"
+print("requesting crumb from "+crumb_request_url)
+crumb_resp = requests.post(crumb_request_url, headers=headers)
+#TODO: check if crumb req successful
+crumb=crumb_resp.json()["crumb"]
+print("received Jenkins crumb "+crumb)
+
+headers["Jenkins-Crumb"] = crumb 
+
+#TODO: get parsed_repo_name and parsed_repo_owner from git url using "import parser from git_url_parse"
+
+#TODO: use sprout lib to render job creation xml
+#ctx = SimpleTemplateContext({'template_jenkins_folder': JENKINS_FOLDER_NAME})
+#renderer = TemplateRenderer(template_context=ctx)
+#result, values, error = renderer.render('test_data/test_template.txt.j2')
+#print(f"Template rendered. Result: '%s' | Return values: %s", result, values)
+
+headers["Content-Type"] = "text/xml"
+
+#TODO: use sprout lib to render cred creation xml & send AWS creds to Jenkins cred store
+#POST credentials.xml to $JENKINS_URL/<path to context>/credentials/store/<store id>/domain/<domain name>/createCredentials
+
+#TODO: parse repo name from git url using "import parser from git_url_parse"
+
+job_creation_url = JENKINS_SERVER_URL + "/job/"+PROJECT_NAME+"/createItem?name=parsed_repo_name"
+with open(filename) as xml:
+    resp = requests.post(job_creation_url, headers=headers, data=xml)
+    print("Job creation HTTP response code: "+str(resp.status_code))
